94ca7f23009f2a19339580c4cebef535b84eba2d
sirodeneko <2487252242@qq.com>
2019-08-28 10:58:19 +0800

    分页

--- service/list_video_service.go
+++ service/list_video_service.go
@@ -5,22 +5,36 @@ import (
 	"giligili/serializer"
 )
 
-// ListVideoService 视频列表
-type ListVideoService struct { //将前端的数据绑定到结构体内
+// ListVideoService 视频列表服务
+type ListVideoService struct {
+	Limit int `form:"limit"`
+	Start int `form:"start"`
 }
 
 // List 视频列表
 func (service *ListVideoService) List() serializer.Response {
-	var videos []model.Video
-	err := model.DB.Find(&videos).Error
-	if err != nil {
+	videos := []model.Video{}
+	total := 0
+
+	if service.Limit == 0 {
+		service.Limit = 12
+	}
+
+	if err := model.DB.Model(model.Video{}).Count(&total).Error; err != nil {
 		return serializer.Response{
 			Status: 50000,
 			Msg:    "数据库连接错误",
 			Error:  err.Error(),
 		}
 	}
-	return serializer.Response{
-		Data: serializer.BuildVideos(videos),
+
+	if err := model.DB.Limit(service.Limit).Offset(service.Start).Find(&videos).Error; err != nil {
+		return serializer.Response{
+			Status: 50000,
+			Msg:    "数据库连接错误",
+			Error:  err.Error(),
+		}
 	}
+
+	return serializer.BuildListResponse(serializer.BuildVideos(videos), uint(total))
 }
